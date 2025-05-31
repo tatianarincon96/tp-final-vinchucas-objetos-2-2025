6a6b9354120ab37e921934b91e34558cd1b3b876
Oriana <orianapettinelli04@gmail.com>
2025-05-31 00:45:42 -0300

    ADD: Foto class

---
+++ src/foto/Foto.java
@@ -0,0 +1,23 @@
+package foto;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Foto {
+	private byte[] contenido;
+	
+    public Foto(String url) {
+        try {
+            this.contenido = Files.readAllBytes(Paths.get(url));
+        } catch (IOException e) {
+            e.printStackTrace();
+            this.contenido = new byte[0];
+        }
+    }
+
+    public byte[] getData() {
+        return contenido;
+    }
+
+}
