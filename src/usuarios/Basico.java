7f4fba6a6a10147720ab098c1e4a3e50f448df4e
tatiana.rincon <tatiana.rincon@globant.com>
2025-06-24 16:39:07 -0300

    Agrego ajuste de clase suuario y niveles

--- src/usuarios/Basico.java
+++ src/usuarios/Basico.java
@@ -5,10 +5,13 @@ import java.util.List;
 public class Basico extends NivelState {
 
     @Override
-    public void updateNivel(Usuario usuario) {
-        List<Integer> cantidadMuestrasYOpiniones = obtainMuestrasYOpiniones(usuario);
-        if (cantidadMuestrasYOpiniones.get(0) > 10 && cantidadMuestrasYOpiniones.get(1) > 20) {
-            usuario.setNivelDeUsuario(new Experto());
-        }
+    protected boolean debeCambiarNivel(Usuario usuario) {
+        List<Integer> cantidades = obtainMuestrasYOpinionesEnLosUltimosXDias(usuario, 30);
+        return cantidades.get(0) > 10 && cantidades.get(1) > 20;
+    }
+
+    @Override
+    protected NivelState nuevoNivel() {
+        return new Experto();
     }
 }
